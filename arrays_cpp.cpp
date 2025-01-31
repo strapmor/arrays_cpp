2dd3143a1349c6189528b81525415bd6eff8fb6c
strapmor <dimonmi0612@yandex.ru>
2025-01-31 23:21:11 +0700

    Добавьте файлы проекта.

---
+++ arrays_cpp.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+
+void task1() {
+
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    task1();
+
+    cout << "hello, world!";
+    return 0;
+}
