79213cb6902c88213ee1fb6d661dcef25d3f1eb3
strapmor <dimonmi0612@yandex.ru>
2025-02-01 17:34:08 +0700

    Выполнены первые 3 задания

--- arrays_cpp.cpp
+++ arrays_cpp.cpp
@@ -1,17 +1,92 @@
 ﻿#include <iostream>
+#include <algorithm>
+#include <limits>
+#include <ctime>
 using namespace std;
 
 
-void task1() {
 
+void task1() {
+    srand(time(NULL));
+    cout << "Задание 1\nВывести количество максимальных элементов массива\n";
+    double arr[10];
+
+    for (int i = 0; i < sizeof(arr) / 8; i++)
+        arr[i] = rand() % 6;
+
+    cout << "Элементы массива: ";
+    for (int i = 0; i < sizeof(arr) / 8; i++)
+        cout << arr[i] << " ";
+
+    double max = *max_element(begin(arr), end(arr));
+    cout << endl << "Максимальный элемент массива: " << max << endl;
+
+    int count = 0;
+    for (int i = 0; i < sizeof(arr) / 8; i++) {
+        if (arr[i] == max)
+            count++;
+    }
+    cout << "Количество максимальных элементов массива: " << count << endl;
+}
+
+void task2() {
+    srand(time(NULL));
+    cout << "Задание 2\nВывести номера минимальных элементов массива\n";
+    double arr[10];
+
+    for (int i = 0; i < sizeof(arr) / 8; i++)
+        arr[i] = rand() % 6;
+
+    cout << "Элементы массива: ";
+    for (int i = 0; i < sizeof(arr) / 8; i++)
+        cout << arr[i] << " ";
+
+    double min = *min_element(begin(arr), end(arr));
+    cout << endl << "Минимальный элемент массива: " << min << endl;
+
+    cout << "Номера минимальных элементов массива: ";
+    for (int i = 0; i < sizeof(arr) / 8; i++) {
+        if (arr[i] == min)
+            cout << i << " ";
+    }
+    cout << endl;
+}
+
+void task3() {
+    srand(time(NULL));
+    cout << "Задание 1\nВывести количество максимальных элементов массива\n";
+    double arr[10];
+
+    for (int i = 0; i < sizeof(arr) / 8; i++)
+        arr[i] = rand() % 6 + 1;
+
+    cout << "Элементы массива: ";
+    for (int i = 0; i < sizeof(arr) / 8; i++)
+        cout << arr[i] << " ";
+
+    double max = *max_element(begin(arr), end(arr));
+    cout << endl << "Максимальный элемент массива: " << max << endl;
+
+
+    for (int i = 0; i < sizeof(arr) / 8; i++) {
+        if (arr[i] == max)
+            arr[i] = 0;
+    }
+
+    cout << "Элементы массива после замены нулями максимального элемента: ";
+    for (int i = 0; i < sizeof(arr) / 8; i++)
+        cout << arr[i] << " ";
+    cout << endl;
 }
 
 int main()
 {
     setlocale(LC_ALL, "rus");
 
-    task1();
+    //task1();
+    //task2();
+    task3();
 
-    cout << "hello, world!";
+    system("pause");
     return 0;
 }
